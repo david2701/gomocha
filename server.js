0127d89d82298f1eddc1471b302ef96e76b500af
Amit Ranan <ranan.amit@gmail.com>
2016-03-15 19:21:47 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+function requestHandler(request, response) {
+    response.sendFile(__dirname + '/index.html');
+}
+app.get('/', requestHandler);
+app.listen(4000);
